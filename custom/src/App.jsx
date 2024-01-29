fa806dcf7fc73db5d9b7ff1c6c44fde1f007aa0a
Swasti008 <swastimohanty452@gmail.com>
2024-01-29 11:32:33 +0530

    done

---
+++ custom/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import CustomHook from './component/customHook'
+
+function App() {
+
+  return (
+    <>
+      <CustomHook/>
+    </>
+  )
+}
+
+export default App
